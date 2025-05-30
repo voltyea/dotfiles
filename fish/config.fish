a0fe0a4d3e95a3f5f565cfea87bf58c2901e15c4
voltyea <volty764@gmail.com>
2025-05-30 20:12:26 +0530

    commit

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+ function fish_greeting
+
+end
 starship init fish | source
 enable_transience
+pokego --no-title --random 1-8
