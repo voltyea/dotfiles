fe6f9ff1b76a65c10acd70b3319e431eb3f61765
voltyea <volty764@gmail.com>
2025-07-06 10:48:31 +0530

    jojo

--- dot_config/hypr/executable_eww.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while ! swww query &>/dev/null; do
-  sleep 0.5
-done
-
-eww open clock
--- dot_config/hypr/executable_nwg-dock-hyprland.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while ! swww query &>/dev/null; do
-  sleep 0.5
-done
-
-nwg-dock-hyprland -d -hd 150 -p "right" -mr 10 -i 40 &
