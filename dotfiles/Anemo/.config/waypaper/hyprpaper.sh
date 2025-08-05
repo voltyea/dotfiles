5e7629a2017cec1f91e17f182ce924ec16a0e781
Your Name <you@example.com>
2025-08-05 16:33:56 +0530

    upadted hyprpaper.sh

--- dotfiles/Anemo/.config/waypaper/hyprpaper.sh
+++ dotfiles/Anemo/.config/waypaper/hyprpaper.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
+# Extract the full filename
+filename=$(basename "$1")
+
+# Extract the prefix (part before the first dot)
+prefix="${filename%%.*}"
+
+# Perform actions based on prefix
+case "$prefix" in
+Anemo)
+  rsync -r $HOME/.local/share/Elements/dotfiles/Anemo/ $HOME/
+  sudo mkdir -p /etc/sddm.conf.d/
+  sudo rsync $HOME/.local/share/Elements/sddm/Anemo/sddm.conf /etc/sddm.conf.d/
+  sudo rsync -r $HOME/.local/share/Elements/sddm/Anemo/Anemo/ /usr/share/sddm/themes/
+  ;;
+
+Electro)
+  rsync -r $HOME/.local/share/Elements/dotfiles/Electro/ $HOME/
+  sudo mkdir -p /etc/sddm.conf.d/
+  sudo rsync $HOME/.local/share/Elements/sddm/Electro/sddm.conf /etc/sddm.conf.d/
+  sudo rsync -r $HOME/.local/share/Elements/sddm/Electro/Electro/ /usr/share/sddm/themes/
+  ;;
+
+Cryo)
+  rsync -r $HOME/.local/share/Elements/dotfiles/Cryo/ $HOME/
+  sudo mkdir -p /etc/sddm.conf.d/
+  sudo rsync $HOME/.local/share/Elements/sddm/Cryo/sddm.conf /etc/sddm.conf.d/
+  sudo rsync -r $HOME/.local/share/Elements/sddm/Cryo/Cryo/ /usr/share/sddm/themes/
+  ;;
+
+*) ;;
+esac
+
 # Define the filename (you can modify this path)
 FILE="$HOME/.config/hypr/hyprpaper.conf"
-
-# Check if the file exists
-if [ ! -e "$FILE" ]; then
-  touch "$FILE"
-fi
 
 NEW_WALLPAPER="$1"
 WALLPAPER_RELATIVE="${NEW_WALLPAPER/#$HOME/\$HOME}"
--- dotfiles/Hydro/.config/waypaper/hyprpaper.sh
+++ dotfiles/Hydro/.config/waypaper/hyprpaper.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+# Extract the full filename
+filename=$(basename "$1")
+
+# Extract the prefix (part before the first dot)
+prefix="${filename%%.*}"
+
+# Perform actions based on prefix
+case "$prefix" in
+anemo)
+  rsync -r $HOME/.local/share/Elements/dotfiles/Anemo/ $HOME/
+  # Add actual command here
+  ;;
+
+electro)
+  rsync -r $HOME/.local/share/Elements/dotfiles/Electro/ $HOME/
+  # Add actual command here
+  ;;
+
+*)
+  echo "Unknown prefix '$prefix'. No action taken."
+  ;;
+esac
+
 # Define the filename (you can modify this path)
 FILE="$HOME/.config/hypr/hyprpaper.conf"
-
-# Check if the file exists
-if [ ! -e "$FILE" ]; then
-  touch "$FILE"
-fi
 
 NEW_WALLPAPER="$1"
 WALLPAPER_RELATIVE="${NEW_WALLPAPER/#$HOME/\$HOME}"
